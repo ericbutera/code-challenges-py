313f8723bf4e47925ba8d77b14273e75f1e2e67f
Eric Butera <eric@ericbutera.com>
2022-06-11 18:19:28 -0400

    feat: learn pytest-mock

---
+++ learn/connector.py
@@ -0,0 +1,122 @@
+from array import array
+from cProfile import label
+from turtle import st
+
+
+class ServiceClient:
+    account: str
+    secret: str
+    region: str
+
+    def __init__(self, account: str, secret: str, region: str):
+        self.account = account
+        self.secret = secret
+        self.region = region
+
+
+class S3Client(ServiceClient):
+    def get_buckets(self):
+        return {
+            "buckets": [
+                {"name": "live-service-1", "location": "live-location-1"},
+                {"name": "live-service-2", "location": "live-location-2"},
+            ]
+        }
+
+
+class Boto:
+    def get_client(service: str, account: str, secret: str, region: str):
+        if service == "s3":
+            return S3Client(account, secret, region)
+        raise Exception("Unknown service: " + service)
+
+
+# TODO record type
+class Finding:
+    label: str
+    value: str
+
+    def __init__(self, label: str, value: str) -> None:
+        self.label = label
+        self.value = value
+
+
+class Connector:
+    _findings: array  # [Finding]
+
+    def __init__(self):
+        self._findings = []
+
+    def get_client(self, service: str) -> ServiceClient:
+        return Boto.get_client(service, "account", "secret", "region")
+
+    def run(self):
+        client = self.get_client("s3")
+        buckets = client.get_buckets()
+
+        for bucket in buckets.get("buckets"):
+            name = bucket.get("name")
+            value = bucket.get("location")
+            self.emit(Finding(name, value))
+
+        return buckets
+
+    def emit(self, data: Finding):
+        self._findings.append(data)
+
+
+class ConnectorWithClient:
+    _findings: array  # [Finding]
+
+    def __init__(self):
+        self._findings = []
+
+    def run(self):
+        buckets = self.fetch_buckets()
+        for bucket in buckets.get("buckets"):
+            name = bucket.get("name")
+            value = bucket.get("location")
+            self.emit(Finding(name, value))
+
+    def get_client(self, service: str) -> ServiceClient:
+        """Boto Service Client Factory"""
+        return Boto.get_client(service, "account", "secret", "region")
+
+    def fetch_buckets(self):
+        client = self.get_client("s3")
+        return client.get_buckets()
+
+    def emit(self, data: Finding):
+        self._findings.append(data)
+
+
+class ConnectorClientFactory:
+    def get_client(service: str) -> ServiceClient:
+        """Boto Service Client Factory"""
+        return Boto.get_client(service, "account", "secret", "region")
+
+
+class ConnectorInjection:
+    _findings: array  # [Finding]
+
+    def __init__(self):
+        self._findings = []
+
+    def run(self):
+        buckets = self.fetch_buckets()
+        self.process_buckets(buckets)
+        return True
+
+    def process_buckets(self):
+        buckets = self.fetch_buckets()
+        for bucket in buckets.get("buckets"):
+            name = bucket.get("name")
+            value = bucket.get("location")
+            self.emit(Finding(name, value))
+
+    def fetch_buckets(self):
+        bucket_client = ConnectorClientFactory().get_client("s3")
+        return bucket_client.get_buckets()
+
+    def emit(self, data: Finding):
+        self._findings.append(data)
---
+++ tests/learn/test_connector.py
@@ -0,0 +1,113 @@
+from unittest import mock
+
+import pytest
+
+from learn.connector import Connector, ConnectorWithClient
+
+
+class TestConnector:
+    @pytest.fixture
+    def buckets(self):
+        return {
+            "buckets": [
+                {"name": "mock-bucket-1", "location": "mock-location-1"},
+                {"name": "mock-bucket-2", "location": "mock-location-2"},
+            ]
+        }
+
+    @pytest.fixture()
+    def s3_service_with_buckets(self, buckets):
+        with mock.patch("learn.connector.Boto.get_client") as mock_client:
+            mock_client.return_value.get_buckets.return_value = buckets
+            yield mock_client
+
+    @pytest.fixture()
+    def s3_client(self, buckets):
+        with mock.patch.object(Connector, "get_client") as mock_client:
+            yield mock_client
+
+    def test_scan_bucket_fetches_buckets(self, s3_service_with_buckets, buckets):
+        # this bit of "much too clever" code will wire up get_buckets inside `s3_service_with_buckets`
+        connector = Connector()
+        found = connector.run()
+        assert buckets == found
+
+    def test_scan_bucket_fetches_buckets2(self, s3_client, buckets):
+        s3_client.return_value.get_buckets.return_value = buckets
+
+        connector = Connector()
+        found = connector.run()
+
+        assert buckets == found
+
+    @mock.patch("learn.connector.Boto.get_client")
+    def test_scan_bucket_emits_findings(self, mock_s3_client):
+        data = {
+            "buckets": [
+                {"name": "mock-bucket-1", "location": "mock-location-1"},
+                {"name": "mock-bucket-2", "location": "mock-location-2"},
+            ]
+        }
+
+        connector = Connector()
+        mock_s3_client.return_value.get_buckets.return_value = data
+
+        connector.run()
+
+        assert len(connector._findings) == 2
+
+
+class TestConnectorWithClient:
+    @pytest.fixture
+    def buckets(self):
+        return {
+            "buckets": [
+                {"name": "mock-bucket-3", "location": "mock-location-3"},
+                {"name": "mock-bucket-4", "location": "mock-location-4"},
+            ]
+        }
+
+    @mock.patch("learn.connector.ConnectorWithClient.fetch_buckets")
+    def test_patch_fetch_buckets(self, fetch_buckets, buckets):
+        connector = ConnectorWithClient()
+        fetch_buckets.return_value = buckets
+        connector.run()
+        assert len(connector._findings) == 2
+
+    # TODO fix this
+    # @mock.patch("learn.connector.ConnectorWithClient")
+    # def test_patch_entire_class(self, connector, buckets):
+    #     connector.fetch_buckets.return_value = buckets
+    #     connector.run()
+    #     assert len(connector._findings) == 2
+
+    def test_patch_object_context(self, buckets):
+        with mock.patch.object(
+            ConnectorWithClient, "fetch_buckets", return_value=buckets
+        ):
+            connector = ConnectorWithClient()
+            connector.run()
+            assert len(connector._findings) == 2
+
+    @mock.patch.object(ConnectorWithClient, "fetch_buckets")
+    def test_patch_object_decorator(self, fetch_buckets, buckets):
+        fetch_buckets.return_value = buckets
+        connector = ConnectorWithClient()
+        connector.run()
+        assert len(connector._findings) == 2
+
+    def test_patch_context(self, buckets):
+        with mock.patch(
+            "learn.connector.ConnectorWithClient.fetch_buckets", return_value=buckets
+        ):
+            connector = ConnectorWithClient()
+            connector.run()
+            assert len(connector._findings) == 2
+
+    def test_patch_object_context(self, buckets):
+        with mock.patch.object(
+            ConnectorWithClient, "fetch_buckets", return_value=buckets
+        ):
+            connector = ConnectorWithClient()
+            connector.run()
+            assert len(connector._findings) == 2
